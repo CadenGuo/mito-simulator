da443d1007e3d754347d4b85a4d1e3a875a0611a
Kaitong Guo <kaitong.guo@grabtaxi.com>
2024-07-26 17:51:02 +0800

    add credit

--- src/pages/simulator/NavBar.tsx
+++ src/pages/simulator/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
 import { Colors } from '../../constants';
+import './link.css';
 
 const { Header } = Layout;
 
@@ -22,6 +23,20 @@ const NavBar: React.FC<IProps> = (props: IProps) => {
       >
         <img style={{ marginRight: '0.5rem' }} width={40} src='mitosis_white.svg' />
         Mito Simulator
+      </span>
+      <span
+        className="m-l-sm m-r-l p-t-none m-t-none"
+        style={{
+          float: 'right',
+          fontSize: 16,
+          fontWeight: 400,
+          color: 'rgba(255, 255, 255, 0.85)',
+          lineHeight: '55px',
+        }}
+      >
+        built by&nbsp;
+        <a href="https://twitter.com/C4den994" target="_blank">@C4den</a>
+        &nbsp;with ❤️
       </span>
     </Header >
   );
